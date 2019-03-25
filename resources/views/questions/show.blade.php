617f45a7e7c0e8091e63959a4e80425bf9c5a5a0
Shreyas B1 <shreyas.b1@olacabs.com>
2019-03-25 06:27:26 +0530

    Add FontAwesome Library

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,32 +5,50 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">
-                        <div class="d-flex align-items-center row">
-                            <div class="col-lg-10">
-                                <h1>{{$question->title}}</h1>
-                            </div>
-                            <div class="ml-auto">
-                                <a href="{{route('questions.index')}}" class="btn btn-outline-secondary">Back to All Questions</a>
+                    <div class="card-body">
+                        <div class="card-title">
+                            <div class="d-flex align-items-center row">
+                                <div class="col-lg-10">
+                                    <h1>{{$question->title}}</h1>
+                                </div>
+                                <div class="ml-auto">
+                                    <a href="{{route('questions.index')}}" class="btn btn-outline-secondary">Back to All Questions</a>
+                                </div>
                             </div>
                         </div>
-
-                    </div>
-
-                    <div class="card-body">
-                        {!! $question->body_html !!}
-                        <div class="float-right">
+                        <hr>
+                        <div class="media">
+                            <div class="d-flex flex-column vote-controls">
+                                <a title="Up Vote this Question" class="vote-up">
+                                    <i class="fas fa-caret-up fa-3x"></i>
+                                </a>
+                                <span class="vote-class">1230</span>
+                                <a title="This question is not useful" class="vote-down off">
+                                    <i class="fas fa-caret-down fa-3x"></i>
+                                </a>
+                                <a title="Click to mark it as favourite(Click Again for Undo)" class="favourite mt-2">
+                                    <i class="fas fa-star fa-2x"></i>
+                                    <span class="favourite-count">
+                                        123
+                                    </span>
+                                </a>
+                            </div>
+                            <div class="media-body">
+                                {!! $question->body_html !!}
+                                <div class="float-right">
                                         <span class="text-muted">
                                             Answered {{ $question->created_date }}
                                         </span>
-                            <div class="media mt-2">
-                                <a href="{{$question->user->url}}" class="pr-2">
-                                    <img src="{{$question->user->avatar}}">
-                                </a>
-                                <div class="media-body mt-1">
-                                    <a href="{{$question->user->url}}" class="href">
-                                        {{$question->user->name}}
-                                    </a>
+                                    <div class="media mt-2">
+                                        <a href="{{$question->user->url}}" class="pr-2">
+                                            <img src="{{$question->user->avatar}}">
+                                        </a>
+                                        <div class="media-body mt-1">
+                                            <a href="{{$question->user->url}}" class="href">
+                                                {{$question->user->name}}
+                                            </a>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -48,6 +66,18 @@
                         <hr>
                         @foreach($question->answers as $answer)
                             <div class="media">
+                                <div class="d-flex flex-column vote-controls">
+                                    <a title="Up Vote this Question" class="vote-up">
+                                        <i class="fas fa-caret-up fa-3x"></i>
+                                    </a>
+                                    <span class="vote-class">1230</span>
+                                    <a title="This question is not useful" class="vote-down off">
+                                        <i class="fas fa-caret-down fa-3x"></i>
+                                    </a>
+                                    <a title="Mark this as best answer" class="favourite mt-2">
+                                        <i class="fas fa-check fa-2x"></i>
+                                    </a>
+                                </div>
                                 <div class="media-body">
                                     {!! $answer->body_html !!}
                                     <div class="float-right">
@@ -66,9 +96,9 @@
                                         </div>
                                     </div>
                                 </div>
-                                <hr>
-                                <br>
                             </div>
+                            <hr>
+                            <br>
                         @endforeach
                     </div>
                 </div>
